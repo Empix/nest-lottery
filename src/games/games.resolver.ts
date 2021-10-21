d946a0e1437a5552dfcda44bcbb7ba3541b705e8
Ádrian <jotinhave@gmail.com>
2021-10-21 10:18:12 -0400

    feat: add guard to queries and mutations of games

--- src/games/games.resolver.ts
+++ src/games/games.resolver.ts
@@ -3,16 +3,20 @@ import { StoreGameInput } from './dto/store-game.input';
 import { GameDTO } from './dto/game.dto';
 import { GamesService } from './games.service';
 import { UpdateGameInput } from './dto/update-game.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Resolver()
 export class GamesResolver {
   constructor(private readonly gamesService: GamesService) {}
 
+  @UseGuards(GqlAuthGuard)
   @Query(() => [GameDTO])
   async findAllGames() {
     return await this.gamesService.findAll();
   }
 
+  @UseGuards(GqlAuthGuard)
   @Query(() => GameDTO)
   async findOneGame(
     @Args('id')
@@ -21,6 +25,7 @@ export class GamesResolver {
     return await this.gamesService.findOne(id);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => GameDTO)
   async storeGame(
     @Args('data')
@@ -29,6 +34,7 @@ export class GamesResolver {
     return await this.gamesService.store(data);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => GameDTO)
   async updateGame(
     @Args('id')
@@ -39,6 +45,7 @@ export class GamesResolver {
     return await this.gamesService.update(id, data);
   }
 
+  @UseGuards(GqlAuthGuard)
   @Mutation(() => Boolean)
   async deleteGame(
     @Args('id')
