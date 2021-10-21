b83b9d5f7beed990921c4608efc5e533348fc0e9
Ádrian <jotinhave@gmail.com>
2021-10-21 09:06:57 -0400

    config: import all modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path';
+
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { GamesModule } from './games/games.module';
+import { BetsModule } from './bets/bets.module';
+import { ForgotPasswordTokensModule } from './forgot-password-tokens/forgot-password-tokens.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      playground: false,
+      autoSchemaFile: join(process.cwd(), '/src/schema.gql'),
+    }),
+    UsersModule,
+    GamesModule,
+    BetsModule,
+    ForgotPasswordTokensModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
