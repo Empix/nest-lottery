080065eabc9bb6b81fa949644281981ed75602c0
Ádrian <jotinhave@gmail.com>
2021-10-21 16:33:18 -0400

    feat: create role based access control

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const ctx = GqlExecutionContext.create(context);
+    const user = ctx.getContext().req.user;
+
+    const isPermitted = requiredRoles.includes(user.role);
+
+    if (!isPermitted) {
+      throw new UnauthorizedException(
+        "You don't have permission to access this route.",
+      );
+    }
+
+    return true;
+  }
+}
