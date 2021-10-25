7367f62196fbfafe6848a6edd50da4e40bd49713
Ádrian <jotinhave@gmail.com>
2021-10-25 15:01:21 -0400

    test: create tests for mail service

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailService } from './mail.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 describe('MailService', () => {
-  let service: MailService;
+  let mailService: MailService;
 
-  beforeEach(async () => {
+  const mockMailerService = {
+    sendMail: jest.fn(),
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailService],
+      providers: [
+        MailService,
+        {
+          provide: MailerService,
+          useValue: mockMailerService,
+        },
+      ],
     }).compile();
 
-    service = module.get<MailService>(MailService);
+    mailService = module.get<MailService>(MailService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  beforeEach(async () => {
+    for (let method in mockMailerService) {
+      mockMailerService[method].mockReset();
+    }
+  });
+
+  it('Should be defined', () => {
+    expect(mailService).toBeDefined();
+  });
+
+  describe('When send email', () => {
+    it('Should call sendMail', async () => {
+      mockMailerService.sendMail.mockResolvedValue(undefined);
+
+      await mailService.sendMail({
+        to: 'example@example.com',
+        subject: 'Test',
+        template: 'test',
+        context: {},
+      });
+
+      expect(mockMailerService.sendMail).toHaveBeenCalledTimes(1);
+      expect(mockMailerService.sendMail).not.toThrowError();
+    });
   });
 });
